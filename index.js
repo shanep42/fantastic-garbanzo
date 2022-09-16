7a717cf0ec99a412ef0861a606b6e8fbb427d558
shanep42 <107292088+shanep42@users.noreply.github.com>
2022-09-16 18:19:07 -0500

    Adds inquirer questions

--- index.js
+++ index.js
@@ -7,14 +7,52 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is your name?',
-            name: 'name',
+            message: 'Enter a project description',
+            name: 'description',
+            default: 'lorem ipsum'
         },
         {
             type: 'input',
-            message: 'What is your quest?',
-            name: 'quest',
+            message: 'Enter installation instructions',
+            name: 'installation',
+            default: 'lorem ipsum'
         },
+        {
+            type: 'input',
+            message: 'Enter instructions for usage',
+            name: 'usage',
+            default: 'lorem ipsum'
+        },
+        {
+            type: 'list',
+            message: 'Select a license:',
+            choices: ['License 1', 'License 2'],
+            name: 'license',
+        },
+        {
+            type: 'input',
+            message: 'Enter info about contributors',
+            name:'contributors',
+            default: "",
+        },
+        {
+            type: 'input',
+            message: 'Enter any info about testing procedures',
+            name: 'tests',
+            default: 'Does not currently use any testing procedures',
+        },
+        {
+            type: 'input',
+            message: 'For contact purposes, what is your github username?',
+            name: 'username',
+            default: 'shanep42',
+        },
+        {
+            type: 'input',
+            message: '... and your preferred email address?',
+            name: 'email',
+            default: 'joe@email.com'
+        }
     ])
     .then((response) => 
         console.log(response)
